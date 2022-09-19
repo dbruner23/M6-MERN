91e3fc0cd11846306694972aa6b98ba4ad417475
nicolashayvice3 <nicolashayvice@gmail.com>
2022-09-19 14:05:29 +1200

    footer start

--- ui/src/pages/Home/Home.tsx
+++ ui/src/pages/Home/Home.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import styles from './Home.module.css'
 
+import { Footer } from '../../components/Footer/Footer'
+
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <div>Home</div>
+      <Footer />
+    </>
   )
 }
 

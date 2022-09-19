1fd26b45cb32b353b8783e156e6c331f219dc19d
nicolashayvice3 <nicolashayvice@gmail.com>
2022-09-19 15:30:01 +1200

    footer titles

--- ui/src/components/Footer/Footer.tsx
+++ ui/src/components/Footer/Footer.tsx
@@ -2,7 +2,25 @@
 export const Footer = () => {
     return (
       <>
-        <div>Footer</div>
+        <footer className="pt-8 fixed w-full bottom-0 left-0 pb-12 px-10 bg-zinc-600">
+            <div className="flex text-white justify-between items-center px-32">
+                <div>
+                    <h2 className="underline text-2xl">
+                        Contact
+                    </h2>
+                </div>
+                <div>
+                    <h2 className="underline text-2xl">
+                        Articles
+                    </h2>
+                </div>
+                <div>
+                    <h2 className="underline text-2xl">
+                        Get Involved
+                    </h2>
+                </div>
+            </div>
+        </footer>
       </>
     );
 };

6648c6167c9c26368bdb1d53a60334e9a098f1d2
nicolashayvice3 <nicolashayvice@gmail.com>
2022-09-20 14:20:19 +1200

    header visual start

---
+++ ui/src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import headerTitle from "../../assets/header/headerTitle.png";
+import searchIcon from "../../assets/header/search.png";
+
+export const Header = () => {
+
+    return (
+      <>
+        <header className="pt-12 fixed w-full top-0 left-0 bg-[#4b4b4b]">
+            hi there i am the header
+        </header>
+      </>
+    );
+};
